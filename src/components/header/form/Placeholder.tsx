e39c318a239108a163ed0905c6b653181c956900
Jakub Zukal <jakub@2bgr.com>
2024-01-24 14:41:41 +0100

    fix: remove async from Placeholder fn definition

--- src/components/header/form/Placeholder.tsx
+++ src/components/header/form/Placeholder.tsx
@@ -1,8 +1,4 @@
-export default async function Placeholder({
-  placeholder,
-}: {
-  placeholder: string;
-}) {
+export default function Placeholder({ placeholder }: { placeholder: string }) {
   return (
     <p className="absolute inset-y-0 start-0 items-center p-2 text-slate-500">
       {placeholder}
