963fd2e014cbd05a83b45d576971168bf7f4551f
Jakub Zukal <jakub@2bgr.com>
2024-01-27 17:48:54 +0100

    feat: redesign TagForm

--- src/components/tags/TagForm.tsx
+++ src/components/tags/TagForm.tsx
@@ -10,27 +10,33 @@ export default function TagForm() {
   const [stagedTags, setStagedTags] = useState<string[]>([]);
   const [inputTag, setInputTag] = useState<string>("");
 
-  // TODO: add links for log and how to host reply
   return (
-    <form action={formAction} className="" onSubmit={() => setStagedTags([])}>
-      <div className="rounded-t bg-slate-700 p-2 text-white">
-        <h1>Rules:</h1>
+    <form
+      action={formAction}
+      className="w-full"
+      onSubmit={() => setStagedTags([])}
+    >
+      <h1 className="text-tertiary text-2xl">Form:</h1>
+      <div className="bg-tertiary text-quaternary rounded-t p-2">
+        <h1 className="text-primary">Rules:</h1>
         <ul className="list-inside list-decimal">
-          <li>strings only</li>
+          <li>lowercase strings only</li>
           <li>use underscore as separator (small_example), if necessary</li>
           <li>no bullshit (your github acc is paired with ops, see log)</li>
           <li>do not cry on reset, host reply yourself</li>
         </ul>
       </div>
       <div className="flex w-full flex-row">
-        <div className="flex h-10 w-10 items-center justify-center rounded-l bg-white text-3xl text-slate-500">
+        <div
+          className={`flex h-10 w-10 items-center justify-center ${stagedTags.length == 0 ? "rounded-bl" : null} text-tertiary bg-white text-3xl`}
+        >
           #
         </div>
         <input
           type="text"
           name="clientForm"
-          className="h-10 w-full p-2"
-          placeholder="Type tag; Hit Enter; Repeat; Submit;"
+          className="placeholder:text-tertiary h-10 w-full p-2"
+          placeholder="Type tag here; Hit Enter; Repeat; Submit;"
           value={inputTag}
           onChange={(event) => {
             setInputTag(event.target.value);
@@ -47,15 +53,16 @@ export default function TagForm() {
         />
         <button
           type="submit"
-          className="flex h-10 w-10 items-center justify-center rounded-r bg-white p-2 "
+          className={`bg-primary text-tertiary flex h-10 w-32 items-center justify-center ${stagedTags.length == 0 ? "rounded-br" : null}`}
         >
-          <SearchIcon />
+          Submit tags
         </button>
       </div>
       <input type="hidden" name="tags" value={stagedTags} />
+
       {stagedTags.length > 0 && (
-        <div className="flex flex-col rounded-b bg-slate-700 p-2 text-white">
-          <h1 className="text-2xl">Staged tags:</h1>
+        <div className="bg-primary flex flex-col  rounded-b p-2 text-white">
+          <h1 className=" text-tertiary text-2xl">Staged tags:</h1>
           <div className="flex flex-row flex-wrap gap-2">
             {stagedTags.map((tag) => (
               <StagingTag
