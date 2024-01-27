0dd226bfbadcc7eb31dae3042a13c689bd23e324
Jakub Zukal <jakub@2bgr.com>
2024-01-27 18:01:37 +0100

    feat: add svg icons

---
+++ src/components/icons/svg.tsx
@@ -0,0 +1,151 @@
+export function SearchIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="#4F4A45"
+      strokeWidth="1.5"
+      strokeLinecap="square"
+      strokeLinejoin="bevel"
+    >
+      <circle cx="11" cy="11" r="8"></circle>
+      <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+    </svg>
+  );
+}
+
+export function DeleteIcon({ color = "#4F4A45" }: { color?: string }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke={color}
+      strokeWidth="1.5"
+      strokeLinecap="square"
+      strokeLinejoin="bevel"
+    >
+      <line x1="18" y1="6" x2="6" y2="18"></line>
+      <line x1="6" y1="6" x2="18" y2="18"></line>
+    </svg>
+  );
+}
+
+export function ArrowsUpIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="#4F4A45"
+      strokeWidth="1.5"
+      strokeLinecap="square"
+      strokeLinejoin="bevel"
+    >
+      <path d="M17 11l-5-5-5 5M17 18l-5-5-5 5" />
+    </svg>
+  );
+}
+
+export function ArrowsDownIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="#4F4A45"
+      strokeWidth="1.5"
+      strokeLinecap="square"
+      strokeLinejoin="bevel"
+    >
+      <path d="M7 13l5 5 5-5M7 6l5 5 5-5" />
+    </svg>
+  );
+}
+
+export function HomeIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="30"
+      height="30"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="#ED7D31"
+      strokeWidth="1.5"
+      strokeLinecap="square"
+      strokeLinejoin="bevel"
+    >
+      <path d="M20 9v11a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V9" />
+      <path d="M9 22V12h6v10M2 10.6L12 2l10 8.6" />
+    </svg>
+  );
+}
+
+export function UploadIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="30"
+      height="30"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="#ED7D31"
+      strokeWidth="1.5"
+      strokeLinecap="square"
+      strokeLinejoin="bevel"
+    >
+      <path d="M3 15v4c0 1.1.9 2 2 2h14a2 2 0 0 0 2-2v-4M17 8l-5-5-5 5M12 4.2v10.3" />
+    </svg>
+  );
+}
+
+export function TagIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="30"
+      height="30"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="#ED7D31"
+      strokeWidth="1.5"
+      strokeLinecap="square"
+      strokeLinejoin="bevel"
+    >
+      <line x1="4" y1="9" x2="20" y2="9"></line>
+      <line x1="4" y1="15" x2="20" y2="15"></line>
+      <line x1="10" y1="3" x2="8" y2="21"></line>
+      <line x1="16" y1="3" x2="14" y2="21"></line>
+    </svg>
+  );
+}
+
+export function HamburgerMenu() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="30"
+      height="30"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="#ED7D31"
+      strokeWidth="1.5"
+      strokeLinecap="square"
+      strokeLinejoin="bevel"
+    >
+      <line x1="3" y1="12" x2="21" y2="12"></line>
+      <line x1="3" y1="6" x2="21" y2="6"></line>
+      <line x1="3" y1="18" x2="21" y2="18"></line>
+    </svg>
+  );
+}
