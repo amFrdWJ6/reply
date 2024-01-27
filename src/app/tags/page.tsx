90149a009b41632371f3f5a3a17cf745db748798
Jakub Zukal <jakub@2bgr.com>
2024-01-27 17:45:06 +0100

    feat: upgrade design for /tag route

--- src/app/tags/page.tsx
+++ src/app/tags/page.tsx
@@ -9,15 +9,18 @@ export default async function Tags() {
       return a.name.localeCompare(b.name);
     })
     .map((tag) => (
-      <span key={tag.id} className="rounded-sm bg-white p-1">
+      <span
+        key={tag.id}
+        className="bg-primary border-secondary text-tertiary rounded-sm border p-1"
+      >
         {tag.name}
       </span>
     ));
 
   return (
     <>
-      <div className="flex flex-col">
-        <h1 className="text-2xl">Available tags:</h1>
+      <div className="flex w-full flex-col">
+        <h1 className="text-tertiary text-2xl">Available tags:</h1>
         <div className="flex flex-row flex-wrap gap-2 p-2">{listTags}</div>
       </div>
       <TagForm />
