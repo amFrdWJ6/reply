fc2740cc1ac0272ab5c30012ccdaa04e69013a3e
Jakub Zukal <jakub@2bgr.com>
2024-01-27 20:04:01 +0100

    feat: add index component: render replies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,50 @@
-export default function Home() {
-  return <main className=""></main>;
+import { redirect } from "next/navigation";
+import {
+  GetAllRepliesByTags,
+  GetAllTags,
+  GetLatestReplies,
+} from "@/lib/db/queries";
+import Reply from "@/components/Reply";
+
+async function CheckURLTags(searchedTags: string) {
+  const allTags = await GetAllTags();
+  const validatedTags = allTags
+    .filter((tag) => searchedTags.split(",").includes(tag.name))
+    .map((tag) => tag.name);
+
+  if (validatedTags.length == 0) {
+    redirect("/");
+  }
+  return validatedTags;
+}
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) {
+  const replies =
+    searchParams.tags != undefined
+      ? await GetAllRepliesByTags(await CheckURLTags(searchParams.tags))
+      : await GetLatestReplies();
+
+  if (replies == null) {
+    return (
+      <p className="text-tertiary">
+        H-h. There is a <span className="text-primary text-6xl">null</span> of
+        replies with those tags.
+        <p>Did you break it ?! ಠ_ಠ</p>
+      </p>
+    );
+  }
+
+  const listReplis = replies.map((reply) => (
+    <Reply key={reply.id} reply={reply} />
+  ));
+
+  return (
+    <div className="columns-1 space-y-4 sm:columns-2 lg:columns-3 xl:columns-4">
+      {listReplis}
+    </div>
+  );
 }
