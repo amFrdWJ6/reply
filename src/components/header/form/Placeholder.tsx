139d14078f43583a35f3347c7da173e5db3c9276
Jakub Zukal <jakub@2bgr.com>
2024-01-27 19:03:10 +0100

    fix: calc width, so truncate looks nice in Placeholder

--- src/components/header/form/Placeholder.tsx
+++ src/components/header/form/Placeholder.tsx
@@ -1,6 +1,6 @@
 export default function Placeholder({ placeholder }: { placeholder: string }) {
   return (
-    <p className="text-quaternary absolute inset-y-0 start-0 w-full items-center truncate p-2">
+    <p className="text-quaternary absolute inset-y-0 start-0 w-[calc(100%-24px)] items-center truncate p-2">
       {placeholder}
     </p>
   );
