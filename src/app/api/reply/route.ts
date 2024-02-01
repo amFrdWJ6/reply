8dcf49df8f8523c1ee681bb89bc6c1b1569b508f
Jakub Zukal <jakub@2bgr.com>
2024-02-01 12:59:16 +0100

    feat: add API route for loading reply
    Public folder is loaded at start of the app, so after upload there is no "easy" way to access newly added files. Acces thru API is not nice, but solves it (at least for now)

---
+++ src/app/api/reply/route.ts
@@ -0,0 +1,20 @@
+import { access, constants, readFile } from "fs/promises";
+import { NextRequest, NextResponse } from "next/server";
+
+export const dynamic = "force-dynamic";
+export async function GET(req: NextRequest) {
+  const fileName = req.nextUrl.searchParams.get("f");
+  if (fileName == null) {
+    return NextResponse.json("No file provided", { status: 422 });
+  }
+
+  return await access(["public/uploads", fileName].join("/"), constants.F_OK)
+    .then(async () => {
+      return new NextResponse(
+        await readFile(["public/uploads", fileName].join("/")),
+      );
+    })
+    .catch(async () => {
+      return NextResponse.json("File does not exsits", { status: 404 });
+    });
+}
