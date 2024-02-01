8aa046b14098a98d4fc6e044885b11e79e7004a6
Jakub Zukal <jakub@2bgr.com>
2024-02-01 13:05:20 +0100

    feat: add Suspense for SearchForm

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import Logo from "./Logo";
 import NavMenu from "./NavMenu";
 import SearchForm from "./SearchForm";
@@ -9,13 +10,15 @@ export default async function Header() {
   return (
     <header className="flex h-12 w-full flex-row justify-between p-1 md:max-w-4xl lg:max-w-5xl">
       <Logo />
-      <SearchForm
-        allTags={allTags}
-        placeholder={
-          placeholders[Math.floor(Math.random() * placeholders.length)]
-        }
-      />
-      <NavMenu />
+      <Suspense>
+        <SearchForm
+          allTags={allTags}
+          placeholder={
+            placeholders[Math.floor(Math.random() * placeholders.length)]
+          }
+        />
+        <NavMenu />
+      </Suspense>
     </header>
   );
 }
