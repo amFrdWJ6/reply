cc4b7bb07b7b1383d7d3230b9da003eb11e259f1
Jakub Zukal <jakub@2bgr.com>
2024-02-01 14:59:56 +0100

    fix: return correct status code (400) for null param

--- src/app/api/reply/route.ts
+++ src/app/api/reply/route.ts
@@ -5,7 +5,7 @@ export const dynamic = "force-dynamic";
 export async function GET(req: NextRequest) {
   const fileName = req.nextUrl.searchParams.get("f");
   if (fileName == null) {
-    return NextResponse.json("No file provided", { status: 422 });
+    return NextResponse.json("No file provided", { status: 400 });
   }
 
   return await access(["public/uploads", fileName].join("/"), constants.F_OK)
