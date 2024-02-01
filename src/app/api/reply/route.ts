104039b68f9c7a82a30e4731618bebb37b1b253c
Jakub Zukal <jakub@2bgr.com>
2024-02-01 15:05:14 +0100

    fix: provider better message for bad request on /api/route

--- src/app/api/reply/route.ts
+++ src/app/api/reply/route.ts
@@ -5,7 +5,7 @@ export const dynamic = "force-dynamic";
 export async function GET(req: NextRequest) {
   const fileName = req.nextUrl.searchParams.get("f");
   if (fileName == null) {
-    return NextResponse.json("No file provided", { status: 400 });
+    return NextResponse.json("No file provided. API call should be /api/reply?f=file.png", { status: 400 });
   }
 
   return await access(["public/uploads", fileName].join("/"), constants.F_OK)
