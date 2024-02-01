75866336744fd8271740ab836657ef7b5b85393c
Jakub Zukal <jakub@2bgr.com>
2024-02-01 16:57:47 +0100

    refactor: rename state/optimise NavMenu
    Rename state to reflect it is just for hamburger menu
    Remove generation of menu, make it static. It did not make sense as it is not a dynamic menu

--- src/components/header/NavMenu.tsx
+++ src/components/header/NavMenu.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Link from "next/link";
+import { useState } from "react";
 import {
   DeleteIcon,
   HamburgerMenu,
@@ -8,37 +9,47 @@ import {
   TagIcon,
   UploadIcon,
 } from "../icons/svg";
-import { useState } from "react";
 
-const links = new Map<JSX.Element, string>([
-  [<HomeIcon key={1} />, "/"],
-  [<UploadIcon key={2} />, "/upload"],
-  [<TagIcon key={3} />, "/tags"],
-]);
+function Menu() {
+  return (
+    <>
+      <Link href={`/`}>
+        <HomeIcon />
+      </Link>
+      <Link href={`/upload`}>
+        <UploadIcon />
+      </Link>
+      <Link href={`/tags`}>
+        <TagIcon />
+      </Link>
+    </>
+  );
+}
 
 export default function NavMenu() {
-  const [isMenuOpen, setMenuState] = useState<boolean>(false);
-  const menu = [...links.entries()].map(([name, url]) => (
-    <Link key={url} href={url}>
-      {name}
-    </Link>
-  ));
+  const [isHamburgerMenuOpen, setHamburgerMenuState] = useState<boolean>(false);
+
   return (
     <nav className="relative right-1 top-0 flex flex-row items-center gap-2">
       <div
         className="flex flex-col rounded-md border-2 border-primary md:hidden"
-        onClick={() => setMenuState(!isMenuOpen)}
+        onClick={() => setHamburgerMenuState(!isHamburgerMenuOpen)}
       >
-        {isMenuOpen ? (
+        {isHamburgerMenuOpen ? (
           <>
             <DeleteIcon color="#ED7D31" size={30} />
-            <div className="fixed top-14 z-30 flex flex-col gap-4">{menu}</div>
+            <div className="fixed top-14 z-30 flex flex-col gap-4">
+              <Menu />
+            </div>
           </>
         ) : (
           <HamburgerMenu />
         )}
       </div>
-      <div className="hidden flex-row gap-2 md:flex">{menu}</div>
+
+      <div className="hidden flex-row gap-2 md:flex">
+        <Menu />
+      </div>
     </nav>
   );
 }
