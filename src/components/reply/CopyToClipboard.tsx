4e96547b35bb816d89bbada084cac1ab13912fc1
Jakub Zukal <jakub@2bgr.com>
2024-02-02 14:02:32 +0100

    feat: use svg icon for CopyToClipboard
    Replace text with svg icon and add title for button

--- src/components/reply/CopyToClipboard.tsx
+++ src/components/reply/CopyToClipboard.tsx
@@ -1,13 +1,16 @@
 "use client";
 
+import { CopyLinkIcon } from "../icons/svg";
+
 export default function CopyToClipboard({ url }: { url: string }) {
   return (
     <button
       onClick={() => {
         navigator.clipboard.writeText(url);
       }}
+      title="Copy to Clipboard"
     >
-      Copy URL
+      <CopyLinkIcon />
     </button>
   );
 }
