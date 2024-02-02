854956e1efbb53841b3baa7e3cbc35de35a611d9
Jakub Zukal <jakub@2bgr.com>
2024-02-02 14:09:51 +0100

    refactor: separate type in Reply

--- src/components/Reply.tsx
+++ src/components/Reply.tsx
@@ -2,28 +2,22 @@ import Image from "next/image";
 import Footer from "./reply/Footer";
 import { ImageLoader } from "@/lib/utils";
 
-export default async function Reply({
-  reply,
-}: {
-  reply: {
-    id: number;
-    title: string;
-    fileName: string;
-    tags: string;
-  };
-}) {
+type TReply = {
+  id: number;
+  title: string;
+  fileName: string;
+  tags: string;
+};
+
+export default async function Reply({ reply }: { reply: TReply }) {
   const src_path = await ImageLoader(reply.fileName);
 
   return (
-    <figure
-      key={reply.id}
-      className="relative h-auto w-64 break-inside-avoid shadow-lg"
-    >
+    <figure key={reply.id} className="relative h-auto w-64 break-inside-avoid">
       <figcaption className="absolute top-0 w-full overflow-hidden truncate text-nowrap bg-primary p-1 text-sm">
         {reply.title}
       </figcaption>
       <Image src={src_path} alt="" width={256} height={256} />
-
       <Footer url={src_path} tags={reply.tags.split(",")} />
     </figure>
   );
