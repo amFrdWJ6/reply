9e3c2d16b01966c14f9e1729e944ac6895c6e4f8
Jakub Zukal <jakub@2bgr.com>
2024-02-02 14:08:05 +0100

    feat: replace text with ArrowIcons component

--- src/components/reply/Footer.tsx
+++ src/components/reply/Footer.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import CopyToClipboard from "./CopyToClipboard";
 import Tags from "./Tags";
+import ArrowIcons from "../header/form/ArrowIcons";
 
 export default function Footer({
   url,
@@ -18,7 +19,9 @@ export default function Footer({
       <div className="flex flex-row justify-between bg-tertiary px-2 py-1 text-primary">
         <CopyToClipboard url={url} />
 
-        <button onClick={() => setTagsState(!areTagsOpen)}>Show Tags</button>
+        <button onClick={() => setTagsState(!areTagsOpen)}>
+          <ArrowIcons size={20} color="#ED7D31" isMenuOpen={areTagsOpen} />
+        </button>
       </div>
       {areTagsOpen && <Tags tags={tags} />}
     </>
