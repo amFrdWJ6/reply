ede8af415da7cae9414a3cc60c2326c9e696f942
Jakub Zukal <jakub@2bgr.com>
2024-02-05 18:09:35 +0100

    feat: add config, API and SessionProvider for auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import { auth } from "@/lib/auth";
+import { SessionProvider } from "next-auth/react";
 import type { Metadata } from "next";
 import { Lato } from "next/font/google";
 import "./globals.css";
@@ -14,20 +16,23 @@ export const metadata: Metadata = {
   description: "Reply to anyone as true memelord",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await auth();
   return (
     <html lang="en">
       <body
         className={`${lato.className} flex w-full flex-col items-center justify-center bg-quaternary`}
       >
-        <Header />
-        <main className="z-0 flex w-full max-w-5xl flex-col items-center p-2">
-          {children}
-        </main>
+        <SessionProvider session={session}>
+          <Header />
+          <main className="z-0 flex w-full max-w-5xl flex-col items-center p-2">
+            {children}
+          </main>
+        </SessionProvider>
       </body>
     </html>
   );
