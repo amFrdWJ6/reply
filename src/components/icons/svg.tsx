c487aea10b46ceaa63fe3357f3edd0e45132db10
Jakub Zukal <jakub@2bgr.com>
2024-02-05 18:49:24 +0100

    feat: add pagination
    Add for log. For replies will be committed soon

--- src/components/icons/svg.tsx
+++ src/components/icons/svg.tsx
@@ -248,6 +248,56 @@ export function LogIcon() {
   );
 }
 
+export function PrevPageIcon({
+  color = "#ED7D31", // "#4F4A45",
+  size = 30,
+}: {
+  color?: string;
+  size?: number;
+}) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke={color}
+      strokeWidth="1.5"
+      strokeLinecap="square"
+      strokeLinejoin="bevel"
+    >
+      <title>Previous page</title>
+      <path d="M19 12H6M12 5l-7 7 7 7" />
+    </svg>
+  );
+}
+
+export function NextPageIcon({
+  color = "#ED7D31",
+  size = 30,
+}: {
+  color?: string;
+  size?: number;
+}) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke={color}
+      strokeWidth="1.5"
+      strokeLinecap="square"
+      strokeLinejoin="bevel"
+    >
+      <title>Next page</title>
+      <path d="M5 12h13M12 5l7 7-7 7" />
+    </svg>
+  );
+}
+
 export function LoadingIcon() {
   return (
     <svg
