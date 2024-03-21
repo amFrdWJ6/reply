e56778d464753ae9e8ee8b7874e3bea697ab03f2
Jakub Zukal <jakub@2bgr.com>
2024-03-21 21:18:30 +0100

    refactor: improve code readability

--- src/app/log/[page]/page.tsx
+++ src/app/log/[page]/page.tsx
@@ -1,42 +1,67 @@
+import { redirect } from "next/navigation";
 import Pagination from "@/components/common/Pagination";
 import { getLogs } from "@/lib/db/queries";
 import { ImageLoader } from "@/lib/utils";
 
+const When = ({ when }: { when: string }) => (
+  <span className="pr-2 text-secondary">{when}</span>
+);
+
+const Who = ({ who }: { who: string }) => (
+  <a href={`https://github.com/${who}`} className="text-primary">
+    {who}
+  </a>
+);
+
+const What = ({ what }: { what: string }) => (
+  <span className="px-2">{what}</span>
+);
+
+const Tag = ({ tag }: { tag: string }) => (
+  <a
+    href={`${process.env.AUTH_URL ?? "http://localhost:3000"}/?tags=${tag}`}
+    className="text-primary"
+  >
+    {tag}
+  </a>
+);
+
+const File = ({ file }: { file: string }) => (
+  <a href={ImageLoader(file)} className="text-primary">
+    {file}
+  </a>
+);
+
 export default async function Log({ params }: { params: { page: number } }) {
-  const logs = await getLogs(params.page);
+  if (!(+params.page >= 0)) {
+    redirect("/log/0");
+  }
+
+  const logs = await getLogs(+params.page);
   const nextPageLogs = await getLogs(+params.page + +1);
 
-  const listOfLogs = logs.map((log) => {
+  const whoDidWhatList = logs.map((log) => {
     return (
-      <p key={log.id}>
-        <span className="pr-2 text-secondary">{log.when}</span>
-
-        <a href={`https://github.com/${log.who}`} className="text-primary">
-          {log.who}
-        </a>
-
-        <span className="px-2">{log.action}</span>
+      <div key={log.id}>
+        <When when={log.when} />
+        <Who who={log.who} />
+        <What what={log.action} />
 
         {log.tag ? (
-          <a
-            href={`http://localhost:3000/?tags=${log.tag}`}
-            className="text-primary"
-          >
-            {log.tag}
-          </a>
+          <Tag tag={log.tag} />
+        ) : log.file ? (
+          <File file={log.file} />
         ) : (
-          <a href={ImageLoader(log.file!)} className="text-primary">
-            {log.file}
-          </a>
+          <p>nor Tag or File provided</p>
         )}
-      </p>
+      </div>
     );
   });
 
   return (
     <div className="flex w-full flex-col">
       <h1 className="text-xl text-primary">Logs</h1>
-      {listOfLogs}
+      {whoDidWhatList}
       <Pagination
         urlPath="log"
         currentPage={params.page}
