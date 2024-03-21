9e9afbfdc161cdf467a130ca9d04ef131b357e3a
Jakub Zukal <jakub@2bgr.com>
2024-03-21 22:16:04 +0100

    refactor: rename Pagination component

---
+++ src/components/common/ServerPagination.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import { NextPageIcon, PrevPageIcon } from "../icons/svg";
+
+export default async function Pagination({
+  urlPath,
+  currentPage,
+  prevPage = false,
+  nextPage = false,
+}: {
+  urlPath: string;
+  currentPage: number;
+  prevPage: boolean;
+  nextPage: boolean;
+}) {
+  return (
+    <div className="flex flex-row items-center justify-center gap-4 p-2">
+      {prevPage ? (
+        <Link href={`/${urlPath}/${currentPage - 1}`}>
+          <PrevPageIcon />
+        </Link>
+      ) : (
+        <PrevPageIcon color="#4F4A45" />
+      )}
+      <span>{currentPage}</span>
+      {nextPage ? (
+        <Link href={`/${urlPath}/${+currentPage + +1}`}>
+          <NextPageIcon />
+        </Link>
+      ) : (
+        <NextPageIcon color="#4F4A45" />
+      )}
+    </div>
+  );
+}
