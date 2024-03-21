83fcadf7015db3154772032b38092852de0635ac
Jakub Zukal <jakub@2bgr.com>
2024-03-21 19:21:42 +0100

    refactor: mv handleUploadForm into own file

---
+++ src/components/upload/actions/handleUploadForm.ts
@@ -0,0 +1,47 @@
+"use server";
+import { getTagsIDs } from "@/lib/db/queries";
+import { isFileFormatAllowed, isURLFileFormatAllowed } from "@/lib/utils";
+import { createDBRecords } from "./createDBRecords";
+import { downloadFile } from "./downloadFile";
+import { uploadFile } from "./uploadFile";
+
+export async function handleUploadForm(prev: any, formData: FormData) {
+  const title: string = formData.get("title") as string;
+  const formTags: string = formData.get("tags") as string;
+  const upload: File | string = formData.get("upload") as File | string;
+  const user: string = formData.get("user") as string;
+
+  const destSrc = process.env.UPLOADS_DIR;
+  if (destSrc == undefined) {
+    throw new Error("Missing ENV variable: UPLOADS_DIR");
+  }
+
+  const tags = await getTagsIDs(formTags.split(","));
+
+  if (typeof upload === "string") {
+    if (!isURLFileFormatAllowed(upload)) {
+      return {
+        type: "error",
+        message: `${upload.split(".").pop()} is not allowed format`,
+      };
+    }
+    const result = await downloadFile(upload, destSrc);
+    if (result.type === "success") {
+      return await createDBRecords(title, result.fileName, user, tags);
+    } else {
+      return result;
+    }
+  }
+
+  if (upload instanceof File) {
+    if (!isFileFormatAllowed(upload)) {
+      return { type: "error", message: `${upload.type} is not allowed format` };
+    }
+    const result = await uploadFile(upload, destSrc);
+    if (result.type === "success") {
+      return await createDBRecords(title, result.fileName, user, tags);
+    } else {
+      return result;
+    }
+  }
+}
--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -17,46 +17,6 @@ import { RTag } from "./db/schema";
 import { isFileFormatAllowed, isURLFileFormatAllowed } from "./utils";
 import { signIn, signOut } from "@/auth";
 
-export async function handleUploadForm(prev: any, formData: FormData) {
-  const title: string = formData.get("title") as string;
-  const formTags: string = formData.get("tags") as string;
-  const upload: File | string = formData.get("upload") as File | string;
-  const user: string = formData.get("user") as string;
-
-  const destSrc = process.env.UPLOADS_DIR;
-  if (destSrc == undefined) {
-    throw new Error("Missing ENV variable: UPLOADS_DIR");
-  }
-
-  const tags = await GetTagsIDs(formTags.split(","));
-
-  if (typeof upload === "string") {
-    if (!isURLFileFormatAllowed(upload)) {
-      return {
-        type: "error",
-        message: `${upload.split(".").pop()} is not allowed format`,
-      };
-    }
-    const result = await DownloadFile(upload, destSrc);
-    if (result.type === "success") {
-      return await CreateDBRecords(title, result.fileName, user, tags);
-    } else {
-      return result;
-    }
-  }
-
-  if (upload instanceof File) {
-    if (!isFileFormatAllowed(upload)) {
-      return { type: "error", message: `${upload.type} is not allowed format` };
-    }
-    const result = await UploadFile(upload, destSrc);
-    if (result.type === "success") {
-      return await CreateDBRecords(title, result.fileName, user, tags);
-    } else {
-      return result;
-    }
-  }
-}
 // Auth
 export async function handleSignIn() {
   return await signIn("github");
