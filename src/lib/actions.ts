c87b52abba12759048bf51f2ffb25aa377abc231
Jakub Zukal <jakub@2bgr.com>
2024-03-21 19:22:05 +0100

    chore: clean up actions.ts

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,20 +1,5 @@
 "use server";
 
-import axios, { AxiosError, AxiosResponse } from "axios";
-import { createWriteStream } from "fs";
-import { unlink, writeFile } from "fs/promises";
-import { redirect } from "next/navigation";
-import { revalidatePath } from "next/cache";
-import { v4 as uuidv4 } from "uuid";
-import { DOWNLOAD_TIMEOUT, MAX_FILE_SIZE_BYTES } from "./const";
-import {
-  AddTagsToReply,
-  CreateLog,
-  CreateReply,
-  GetTagsIDs,
-} from "./db/queries";
-import { RTag } from "./db/schema";
-import { isFileFormatAllowed, isURLFileFormatAllowed } from "./utils";
 import { signIn, signOut } from "@/auth";
 
 // Auth
