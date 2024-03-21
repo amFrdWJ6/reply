7ecd0a98906ea0d6ca3a0ecb05f7d3bcd8c2660e
Jakub Zukal <jakub@2bgr.com>
2024-03-21 11:40:06 +0100

    chore: slightly more readable code in Header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -18,17 +18,14 @@ async function HeaderSkeleton() {
 export default async function Header() {
   const session = await auth();
   const allTags = await GetAllTags();
+  const placeholder =
+    placeholders[Math.floor(Math.random() * placeholders.length)];
+
   return (
     <header className="flex h-12 w-full flex-row justify-between p-1 md:max-w-4xl lg:max-w-5xl">
       <Logo />
       <Suspense fallback={<HeaderSkeleton />}>
-        <SearchForm
-          allTags={allTags}
-          placeholder={
-            placeholders[Math.floor(Math.random() * placeholders.length)]
-          }
-        />
-        <NavMenu />
+        <SearchForm allTags={allTags} placeholder={placeholder} />
         <NavMenu session={session} />
       </Suspense>
     </header>
