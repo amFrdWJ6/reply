f75a946b51977e0db0cde98594877624933da6cd
Jakub Zukal <jakub@2bgr.com>
2024-03-21 11:29:02 +0100

    feat: provider suspense skeleton for header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,12 +5,21 @@ import SearchForm from "./SearchForm";
 import { placeholders } from "@/lib/const";
 import { GetAllTags } from "@/lib/db/queries";
 
+async function HeaderSkeleton() {
+  return (
+    <>
+      <div></div>
+      <nav></nav>
+    </>
+  );
+}
+
 export default async function Header() {
   const allTags = await GetAllTags();
   return (
     <header className="flex h-12 w-full flex-row justify-between p-1 md:max-w-4xl lg:max-w-5xl">
       <Logo />
-      <Suspense>
+      <Suspense fallback={<HeaderSkeleton />}>
         <SearchForm
           allTags={allTags}
           placeholder={
