0c8d7caf015cd7216d437b52125a93c26f996781
Jakub Zukal <jakub@2bgr.com>
2024-03-22 16:01:56 +0100

    feat: add ClientPagination

--- src/components/common/ClientPagination.tsx
+++ src/components/common/ClientPagination.tsx
@@ -1,31 +1,48 @@
-import Link from "next/link";
+"use client";
+
+import { MouseEventHandler, useState } from "react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { NextPageIcon, PrevPageIcon } from "../icons/svg";
 
-export default async function Pagination({
-  urlPath,
-  currentPage,
-  prevPage = false,
-  nextPage = false,
-}: {
-  urlPath: string;
-  currentPage: number;
-  prevPage: boolean;
-  nextPage: boolean;
-}) {
+const getPage = (page: string | null): number => {
+  return page != null && /^[0-9]*$/.test(page) ? parseInt(page) : 0;
+};
+
+export default function ClientPagination({ more }: { more: boolean }) {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const [page, setPage] = useState<number>(() => {
+    return getPage(searchParams.get("page"));
+  });
+
+  const handleRoutingPage = (newPage: number) => {
+    const params = new URLSearchParams(searchParams);
+
+    setPage(newPage);
+    params.set("page", newPage.toString());
+
+    if (searchParams.get("tags")) {
+      params.set("tags", searchParams.get("tags")!);
+    }
+    replace(`${pathname}?${params.toString()}`);
+  };
+
   return (
     <div className="flex flex-row items-center justify-center gap-4 p-2">
-      {prevPage ? (
-        <Link href={`/${urlPath}/${currentPage - 1}`}>
+      {page > 0 ? (
+        <button onClick={() => handleRoutingPage(page - 1)}>
           <PrevPageIcon />
-        </Link>
+        </button>
       ) : (
         <PrevPageIcon color="#4F4A45" />
       )}
-      <span>{currentPage}</span>
-      {nextPage ? (
-        <Link href={`/${urlPath}/${+currentPage + +1}`}>
+      <span>{page}</span>
+      {page === 0 && more ? (
+        <button onClick={() => handleRoutingPage(page + 1)}>
           <NextPageIcon />
-        </Link>
+        </button>
       ) : (
         <NextPageIcon color="#4F4A45" />
       )}
